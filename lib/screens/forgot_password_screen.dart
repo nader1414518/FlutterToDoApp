2f9fdd0f1894f6ae4cb7ae47467d4a4682459774
Nader Sayed <49884739+nader1414518@users.noreply.github.com>
2024-11-16 20:03:17 +0200

    Forgot Password

---
+++ lib/screens/forgot_password_screen.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+import 'package:to_do_app/controllers/auth_controller.dart';
+import 'package:to_do_app/utils/assets_utils.dart';
+
+class ForgotPasswordScreen extends StatefulWidget {
+  @override
+  ForgotPasswordScreenState createState() => ForgotPasswordScreenState();
+}
+
+class ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
+  bool isLoading = false;
+
+  TextEditingController emailController = TextEditingController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: isLoading
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView(
+              padding: const EdgeInsets.symmetric(
+                vertical: 20,
+                horizontal: 20,
+              ),
+              shrinkWrap: true,
+              physics: const ClampingScrollPhysics(),
+              children: [
+                const SizedBox(
+                  height: 60,
+                ),
+                Container(
+                  width: (MediaQuery.sizeOf(context).width - 40) * 0.6,
+                  height: (MediaQuery.sizeOf(context).width - 40) * 0.6,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(
+                        AssetsUtils.appLogo,
+                      ),
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(
+                        15,
+                      ),
+                    ),
+                    labelText: "Email",
+                    isDense: true,
+                    contentPadding: const EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 10,
+                    ),
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                  controller: emailController,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextButton(
+                      onPressed: () async {
+                        if (emailController.text == "") {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text(
+                                "Please enter your email!!",
+                              ),
+                            ),
+                          );
+                          return;
+                        }
+
+                        setState(() {
+                          isLoading = true;
+                        });
+
+                        try {
+                          var res = await AuthController.forgotPasswordCallback(
+                            emailController.text,
+                          );
+
+                          if (res["result"] == true) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text(
+                                  res["message"].toString(),
+                                ),
+                              ),
+                            );
+
+                            Navigator.of(context).pop();
+                          } else {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text(
+                                  res["message"].toString(),
+                                ),
+                              ),
+                            );
+                          }
+                        } catch (e) {
+                          print(e.toString());
+                        }
+
+                        setState(() {
+                          isLoading = false;
+                        });
+                      },
+                      child: const Text(
+                        "Reset Password",
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+    );
+  }
+}
